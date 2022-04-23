06d1128dbfb70a0fbe7bba265d067840531ea094
Xy <khoury.daniel@outlook.com>
2022-04-23 20:30:50 +0200

    fait l'anim mais un peu wonky

--- 04-gapminder/src/index.js
+++ 04-gapminder/src/index.js
@@ -1,17 +1,49 @@
 import * as d3 from 'd3';
-import incomeData from "/data/income_per_person_gdppercapita_ppp_inflation_adjusted.csv";
-import lifeData from "/data/life_expectancy_years.csv";
+// import incomeData from "/data/income_per_person_gdppercapita_ppp_inflation_adjusted.csv";
+// import lifeData from "/data/life_expectancy_years.csv";
 // Pour importer les données
 // import file from '../data/data.csv'
+// Pour importer les données
+import gdp from '../data/income_per_person_gdppercapita_ppp_inflation_adjusted.csv'
+import lifeExpectancy from '../data/life_expectancy_years.csv'
+import population from '../data/population_total.csv'
 
-//Transformation des données afin qu'elles soient exploitables
-import populationData from '../data/population_total.csv'
+
+
 // Récupère toutes les années
-const annees = Object.keys(populationData[0])
-//console.log(annees)
+const annees = Object.keys(population[0])
+// console.log(annees)
 
-let converterSI = (array, variable, variableName) => {
+let pop = [],
+    income = [],
+    life = [],
+    dataCombined = [];
 
+// Merge data
+const mergeByCountry = (a1, a2, a3) => {
+    let data = [];
+    a1.map(itm => {
+        let newObject = {
+            ...a2.find((item) => (item.country === itm.country) && item),
+            ...a3.find((item) => (item.country === itm.country) && item),
+            ...itm
+        }
+        data.push(newObject);
+    })
+    return data;
+}
+
+annees.forEach(annee => {
+    pop.push({ "annee": annee, "data": converterSI(population, annee, "pop") })
+    income.push({ "annee": annee, "data": converterSI(gdp, annee, "income") })
+    life.push({ "annee": annee, "data": converterSI(lifeExpectancy, annee, "life") })
+    const popAnnee = pop.filter(d => d.annee == annee).map(d => d.data)[0];
+    const incomeAnnee = income.filter(d => d.annee == annee).map(d => d.data)[0];
+    const lifeAnnee = life.filter(d => d.annee == annee).map(d => d.data)[0];
+    dataCombined.push({ "annee": annee, "data": mergeByCountry(popAnnee, incomeAnnee, lifeAnnee) })
+});
+
+function converterSI(array, variable, variableName) {
     let convertedVariable = array.map(d => {
         // Trouver le format SI (M, B, k)
         let SI = typeof d[variable.toString()] === 'string' || d[variable.toString()] instanceof String ? d[variable.toString()].slice(-1) : d[variable.toString()];
@@ -20,19 +52,19 @@ let converterSI = (array, variable, variableName) => {
         // Selon la valeur SI, multiplier par la puissance
         switch (SI) {
             case 'M': {
-                return {"country": d.country, [variableName] : Math.pow(10, 6) * number};
+                return { "country": d.country, [variableName]: Math.pow(10, 6) * number };
                 break;
             }
             case 'B': {
-                return {"country": d.country, [variableName] : Math.pow(10, 9) * number};
+                return { "country": d.country, [variableName]: Math.pow(10, 9) * number };
                 break;
             }
             case 'k': {
-                return {"country": d.country, [variableName]: Math.pow(10, 3) * number};
+                return { "country": d.country, [variableName]: Math.pow(10, 3) * number };
                 break;
             }
             default: {
-                return {"country": d.country, [variableName] : number};
+                return { "country": d.country, [variableName]: number };
                 break;
             }
         }
@@ -40,190 +72,371 @@ let converterSI = (array, variable, variableName) => {
     return convertedVariable;
 };
 
+// DÉVELOPPEMENT INDIVIDUEL :
 
-let pop = [],
-    income = [],
-    life = [],
-dataCombined = [];
-
-// Merge data
-const mergeByCountry = (a1, a2, a3) => {
-    let data = [];
-    a1.map(itm => {
-        let newObject = {
-        ...a2.find((item) => (item.country === itm.country) && item),
-        ...a3.find((item) => (item.country === itm.country) && item),
-        ...itm
-    }
-    data.push(newObject);
+dataCombined.forEach(annee => {
+    annee.data.forEach(pays => {
+        if (isUnknow(pays.pop) || isUnknow(pays.life) || isUnknow(pays.income)) {
+            pays.pop = undefined;
+            pays.life = undefined;
+            pays.income = undefined;
+        }
     })
-    return data;
-}
-
-annees.forEach(annee => {
-    pop.push({"annee":annee, "data" : converterSI(populationData, annee, "pop")})
-    income.push({"annee":annee, "data" : converterSI(incomeData, annee, "income")})
-    life.push({"annee":annee, "data" : converterSI(lifeData, annee, "life")})
-    const popAnnee = pop.filter(d => d.annee == annee).map(d => d.data)[0];
-    const incomeAnnee = income.filter(d => d.annee == annee).map(d => d.data)[0];
-    const lifeAnnee = life.filter(d => d.annee == annee).map(d => d.data)[0];
-    dataCombined.push({"annee": annee, "data": mergeByCountry(popAnnee, incomeAnnee, lifeAnnee)})
 });
 
+function isUnknow(elm) {
+    if (isNaN(elm) || elm == null || elm == undefined) {
+        return true;
+    }
+}
 
-//console.log(dataCombined[221])//2021
+d3.select("body")
+    .append("div")
+    .attr('id', 'graph')
 
-let incomeTab = dataCombined[221].data.map(d=> d.income);
-let maxIncome = d3.max(incomeTab);
+let margin = { top: 10, right: 20, bottom: 30, left: 50 },
+    width = 1000 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
 
-let lifeExpMax = dataCombined[221].data.map(d=> d.life);
-let maxLife = d3.max(lifeExpMax)
+let svg = d3.select("#graph")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+// Générer une taille d'axe X cohérente
+let theBiggestGDP = 0;
+dataCombined.forEach(annee => {
+    annee.data.forEach(pays => {
+        if (pays.income >= theBiggestGDP) {
+            theBiggestGDP = pays.income;
+        }
+    })
+});
 
-let popArray = dataCombined[221].data.map(d=>d.pop);
-let maxPop = d3.max(popArray)
-
-let index = 0;
-
-
-
-
-//CREATION DU GRAPHIQUE--------------------------------
-// set the dimensions and margins of the graph
-var margin = { top: 10, right: 20, bottom: 30, left: 50 },
-  width = 1000 - margin.left - margin.right,
-  height = 420 - margin.top - margin.bottom;
-
-// append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform",
-    "translate(" + margin.left + "," + margin.top + ")");
-
-
-//CREATION DES AXES------------------------------------
 // Add X axis
-var x = d3.scalePow()
-  .domain([0, maxIncome])
-  .range([0, width]);
+let x = d3.scaleSqrt()
+    .domain([0, theBiggestGDP])
+    .range([0, width]);
 svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x));
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+// Générer une taille d'axe Y cohérente
+let theBiggestLifeExp = 0;
+dataCombined.forEach(annee => {
+    annee.data.forEach(pays => {
+        if (pays.life >= theBiggestLifeExp) {
+            theBiggestLifeExp = pays.life;
+        }
+    })
+});
 
 // Add Y axis
-var y = d3.scaleLinear()
-  .domain([40, maxLife])
-  .range([height, 0]);
+let y = d3.scalePow()
+    .exponent(1.25)
+    .domain([0, theBiggestLifeExp * 1.1])
+    .range([height, 0]);
 svg.append("g")
-  .call(d3.axisLeft(y));
+    .call(d3.axisLeft(y));
 
 // Add a scale for bubble size
-var z = d3.scaleSqrt()
-  .domain([0, maxPop])
-  .range([0, 30]);
+let z = d3.scaleLinear()
+    .domain([200000, 1310000000])
+    .range([5, 60]);
 
-  svg.append('g')
-    .selectAll("dot")
-    .data(dataCombined[221].data)
-    .enter()
-    .append("circle")
-      .attr("cx", function (d) { return x(d.income); } )
-      .attr("cy", function (d) { return y(d.life); } )
-      .attr("r", function (d) { return z(d.pop); } )
-      .style("fill", "#69b3a2")
-      .style("opacity", "0.7")
-      .attr("stroke", "black")
+// Variable où on stocke l'id de notre intervalle
+let nIntervId;
+
+function animate() {
+    // regarder si l'intervalle a été déjà démarré
+    if (!nIntervId) {
+        nIntervId = setInterval(play, 100);
+    }
+    console.log(nIntervId);
+}
+
+d3.select('body').append('h1').attr('id', 'anneeCourante')
+
+let i = -1;
+function play() {
+    // Recommencer si à la fin du tableau
+    if (i == 250) {
+        i = 0;
+    } else {
+        i++;
+    }
+
+    d3.select('#anneeCourante').text(dataCombined[i].annee)
+    updateChart(dataCombined[i]);
+}
+
+// Mettre en pause
+function stop() {
+    clearInterval(nIntervId);
+    nIntervId = null;
+}
+
+// Fonction de mise à jour du graphique
+function updateChart(data_iteration) {
+    svg.selectAll('circle')
+        .data(data_iteration.data)
+        .join(enter => enter.append('circle')
+            .attr("stroke", "black")
+            .style("fill", `#${Math.floor(Math.random() * 16777215).toString(16)}70`)
+            .attr('cx', function (d) { return x(d.income); })
+            .attr('cy', function (d) { return y(d.life); }).transition(d3.transition()
+                .duration(500)
+                .ease(d3.easeLinear)).attr('r', function (d) { return z(d.pop); }),
+            update => update.transition(d3.transition()
+                .duration(500)
+                .ease(d3.easeLinear))
+                .attr('r', function (d) { return z(d.pop); })
+                .attr('cx', function (d) { return x(d.income); })
+                .attr('cy', function (d) { return y(d.life); }),
+            exit => exit.remove())
+}
+
+animate();
 
 
-//CREATION DE LA CARTE-----------------------------------------------------
-var svg = d3.select("#my_dataviz")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform",
-    "translate(" + margin.left + "," + margin.top + ")");
 
-  //map & projection
-  //geo path & selection du type de projection
-  let path = d3.geoPath();
-  let projection = d3.geoMercator()
-    .scale(70)
-    .center([0,20])
-    .translate([width / 2, height / 2]);
+
+
+
+
+
+
+//RECENT OLD CODE
+
+// //Transformation des données afin qu'elles soient exploitables
+// import populationData from '../data/population_total.csv'
+// // Récupère toutes les années
+// const annees = Object.keys(populationData[0])
+// //console.log(annees)
+
+// let converterSI = (array, variable, variableName) => {
+
+//     let convertedVariable = array.map(d => {
+//         // Trouver le format SI (M, B, k)
+//         let SI = typeof d[variable.toString()] === 'string' || d[variable.toString()] instanceof String ? d[variable.toString()].slice(-1) : d[variable.toString()];
+//         // Extraire la partie numérique
+//         let number = typeof d[variable.toString()] === 'string' || d[variable.toString()] instanceof String ? parseFloat(d[variable.toString()].slice(0, -1)) : d[variable.toString()];
+//         // Selon la valeur SI, multiplier par la puissance
+//         switch (SI) {
+//             case 'M': {
+//                 return {"country": d.country, [variableName] : Math.pow(10, 6) * number};
+//                 break;
+//             }
+//             case 'B': {
+//                 return {"country": d.country, [variableName] : Math.pow(10, 9) * number};
+//                 break;
+//             }
+//             case 'k': {
+//                 return {"country": d.country, [variableName]: Math.pow(10, 3) * number};
+//                 break;
+//             }
+//             default: {
+//                 return {"country": d.country, [variableName] : number};
+//                 break;
+//             }
+//         }
+//     })
+//     return convertedVariable;
+// };
+
+
+// let pop = [],
+//     income = [],
+//     life = [],
+// dataCombined = [];
+
+// // Merge data
+// const mergeByCountry = (a1, a2, a3) => {
+//     let data = [];
+//     a1.map(itm => {
+//         let newObject = {
+//         ...a2.find((item) => (item.country === itm.country) && item),
+//         ...a3.find((item) => (item.country === itm.country) && item),
+//         ...itm
+//     }
+//     data.push(newObject);
+//     })
+//     return data;
+// }
+
+// annees.forEach(annee => {
+//     pop.push({"annee":annee, "data" : converterSI(populationData, annee, "pop")})
+//     income.push({"annee":annee, "data" : converterSI(incomeData, annee, "income")})
+//     life.push({"annee":annee, "data" : converterSI(lifeData, annee, "life")})
+//     const popAnnee = pop.filter(d => d.annee == annee).map(d => d.data)[0];
+//     const incomeAnnee = income.filter(d => d.annee == annee).map(d => d.data)[0];
+//     const lifeAnnee = life.filter(d => d.annee == annee).map(d => d.data)[0];
+//     dataCombined.push({"annee": annee, "data": mergeByCountry(popAnnee, incomeAnnee, lifeAnnee)})
+// });
+
+
+// //console.log(dataCombined[221])//2021
+
+// let incomeTab = dataCombined[221].data.map(d=> d.income);
+// let maxIncome = d3.max(incomeTab);
+
+// let lifeExpMax = dataCombined[221].data.map(d=> d.life);
+// let maxLife = d3.max(lifeExpMax)
+
+
+// let popArray = dataCombined[221].data.map(d=>d.pop);
+// let maxPop = d3.max(popArray)
+
+// let index = 0;
+
+
+
+
+// //CREATION DU GRAPHIQUE--------------------------------
+// // set the dimensions and margins of the graph
+// var margin = { top: 10, right: 20, bottom: 30, left: 50 },
+//   width = 1000 - margin.left - margin.right,
+//   height = 420 - margin.top - margin.bottom;
+
+// // append the svg object to the body of the page
+// var svg = d3.select("#my_dataviz")
+//   .append("svg")
+//   .attr("width", width + margin.left + margin.right)
+//   .attr("height", height + margin.top + margin.bottom)
+//   .append("g")
+//   .attr("transform",
+//     "translate(" + margin.left + "," + margin.top + ")");
+
+
+// //CREATION DES AXES------------------------------------
+// // Add X axis
+// var x = d3.scalePow()
+//   .domain([0, maxIncome])
+//   .range([0, width]);
+// svg.append("g")
+//   .attr("transform", "translate(0," + height + ")")
+//   .call(d3.axisBottom(x));
+
+// // Add Y axis
+// var y = d3.scaleLinear()
+//   .domain([40, maxLife])
+//   .range([height, 0]);
+// svg.append("g")
+//   .call(d3.axisLeft(y));
+
+// // Add a scale for bubble size
+// var z = d3.scaleSqrt()
+//   .domain([0, maxPop])
+//   .range([0, 30]);
+
+//   svg.append('g')
+//     .selectAll("dot")
+//     .data(dataCombined[221].data)
+//     .enter()
+//     .append("circle")
+//       .attr("cx", function (d) { return x(d.income); } )
+//       .attr("cy", function (d) { return y(d.life); } )
+//       .attr("r", function (d) { return z(d.pop); } )
+//       .style("fill", "#69b3a2")
+//       .style("opacity", "0.7")
+//       .attr("stroke", "black")
+
+
+// //CREATION DE LA CARTE-----------------------------------------------------
+// var svg = d3.select("#my_dataviz")
+//   .append("svg")
+//   .attr("width", width + margin.left + margin.right)
+//   .attr("height", height + margin.top + margin.bottom)
+//   .append("g")
+//   .attr("transform",
+//     "translate(" + margin.left + "," + margin.top + ")");
+
+//   //map & projection
+//   //geo path & selection du type de projection
+//   let path = d3.geoPath();
+//   let projection = d3.geoMercator()
+//     .scale(70)
+//     .center([0,20])
+//     .translate([width / 2, height / 2]);
     
-    const data = new Map();
+//     const data = new Map();
 
     
-let colorScale = d3.scaleThreshold()
-  .domain([100000, 1000000, 10000000, 30000000, 100000000, 50000000, 100000000])
-  .range(d3.schemeBlues[8]);
+// let colorScale = d3.scaleThreshold()
+//   .domain([100000, 1000000, 10000000, 30000000, 100000000, 50000000, 100000000])
+//   .range(d3.schemeBlues[8]);
 
-// Load external data and boot
-Promise.all([
-  d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson", function(d) {
-  })]).then(function(loadData){
-      let topo = loadData[0]
+// // Load external data and boot
+// Promise.all([
+//   d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson", function(d) {
+//   })]).then(function(loadData){
+//       let topo = loadData[0]
     
-      let mouseOver = function(d) {
-        d3.selectAll(".Country")
-          .transition()
-          .duration(200)
-          .style("opacity", .5)
-        d3.select(this)
-          .transition()
-          .duration(200)
-          .style("opacity", 1)
-          .style("stroke", "black")
-      }
+//       let mouseOver = function(d) {
+//         d3.selectAll(".Country")
+//           .transition()
+//           .duration(200)
+//           .style("opacity", .5)
+//         d3.select(this)
+//           .transition()
+//           .duration(200)
+//           .style("opacity", 1)
+//           .style("stroke", "black")
+//       }
     
-      let mouseLeave = function(d) {
-        d3.selectAll(".Country")
-          .transition()
-          .duration(200)
-          .style("opacity", .8)
-        d3.select(this)
-          .transition()
-          .duration(200)
-          .style("stroke", "transparent")
-      }
-      console.log(topo) //topo.features
-      console.log(pop[221])
-      // Draw the map
-      svg.append("g")
-        .selectAll("path")
-        .data(topo.features)
-        .enter()
-        .append("path")
-          // draw each country
-          .attr("d", d3.geoPath()
-            .projection(projection)
-          )
-          .style("stroke","black")
-          // set the color of each country
+//       let mouseLeave = function(d) {
+//         d3.selectAll(".Country")
+//           .transition()
+//           .duration(200)
+//           .style("opacity", .8)
+//         d3.select(this)
+//           .transition()
+//           .duration(200)
+//           .style("stroke", "transparent")
+//       }
+//       console.log(topo) //topo.features
+//       console.log(pop[221])
+//       // Draw the map
+//       svg.append("g")
+//         .selectAll("path")
+//         .data(topo.features)
+//         .enter()
+//         .append("path")
+//           // draw each country
+//           .attr("d", d3.geoPath()
+//             .projection(projection)
+//           )
+//           .style("stroke","black")
+//           // set the color of each country
           
-          .attr("fill", (d,i) => { 
-            let populasse = pop[221].data.find(pop=> pop.country == d.properties.name) || 0 ;
-            console.log(populasse)
-            if (populasse == 0){
-              return('white')
-            } else{
-              return colorScale(populasse.pop);}
+//           .attr("fill", (d,i) => { 
+//             let populasse = pop[221].data.find(pop=> pop.country == d.properties.name) || 0 ;
+//             console.log(populasse)
+//             if (populasse == 0){
+//               return('white')
+//             } else{
+//               return colorScale(populasse.pop);}
             
-          })
+//           })
           
 
-          .attr("class", function(d){ return "Country" } )
-          .style("opacity", .8)
-          .on("mouseover", mouseOver )
-          .on("mouseleave", mouseLeave )
+//           .attr("class", function(d){ return "Country" } )
+//           .style("opacity", .8)
+//           .on("mouseover", mouseOver )
+//           .on("mouseleave", mouseLeave )
     
-    })
-    console.log(pop[221].data)
-  
+//     })
+//     console.log(pop[221].data)
+ 
+    
+
+
+
+
+
+//OLD CODE----------------------------
 // year.forEach(year=>{
 //   //console.log(year)
 //   theTable.push({
